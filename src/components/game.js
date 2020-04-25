213225871682693c0fbfc467064fccf52b7d3e7c
Jacob  Taylor <jacobtaylor@Jacobs-MBP.attlocal.net>
2020-04-25 16:55:24 -0700

    numbers by bombs logic added

--- src/components/game.js
+++ src/components/game.js
@@ -20,6 +20,17 @@ const gameRef = {
     }
 }
 
+const directionRef = {
+    upLeft: {y: -1, x: -1},
+    up: {y: 0, x: -1},
+    upRight: {y: 1, x: -1},
+    right: {y: 1, x: 0},
+    downRight: {y: 1, x: 1},
+    down: {y: 0, x: 1},
+    downLeft: {y: -1, x: 1},
+    left: {y: -1, x: 0},
+}
+
 class Game extends Component {
     constructor(props) {
         super(props)
@@ -57,26 +68,56 @@ class Game extends Component {
 
     addMines = (availableSlots, gameBoard, mines) => {
         while(mines) {
-            const randNum = getRandomNumber(0, availableSlots.length);
-            const x = availableSlots[randNum][0];
-            const y = availableSlots[randNum][1];
-            gameBoard[x][y] = "m";
+            const randNum = getRandomNumber(0, availableSlots.length -1);
+            const y = availableSlots[randNum][0];
+            const x = availableSlots[randNum][1];
+            gameBoard[y][x] = "m";
             availableSlots.splice(randNum, 1);
             mines--;
         }
-        return gameBoard;
+        return this.checkToAddNumbers(gameBoard);
     }
-    
 
+    checkToAddNumbers = (gameBoardWithMines) => {
+        for (let y = 0; y < gameBoardWithMines.length; y++) {
+            for (let x = 0; x < gameBoardWithMines[y].length; x++) {
+                if (gameBoardWithMines[y][x] === "m") {
+                    this.addNumbers(y, x, gameBoardWithMines);
+                }
+            }
+        }
+        return gameBoardWithMines;
+    }
 
+    addNumbers = (y, x, gameBoardWithMines) => {
+        for (const key in directionRef) {
+            const nextX = x + directionRef[key].x;
+            const nextY = y + directionRef[key].y;
+            if (this.checkIfOffBoard(nextY, nextX)) {
+                continue;
+            }
+            // console.log(nextX, nextY, this.gameParams.w, this.gameParams.h)
+            if (gameBoardWithMines[nextY][nextX] === "m") {
+                continue;
+            }
+            if (gameBoardWithMines[nextY][nextX] === " ") {
+                gameBoardWithMines[nextY][nextX] = 1;
+                continue;
+            }
+            gameBoardWithMines[nextY][nextX] = gameBoardWithMines[nextY][nextX] + 1;
+        }
+    }
+
+    checkIfOffBoard = (y, x) => {
+        if (x >= this.gameParams.h || x < 0) return true;
+        if (y >= this.gameParams.w || y < 0) return true;
+        return null;
+    }
 
     render() {
         console.log("STATE", this.state)
-        const { gameBoardWithMines, userViewGameBoard } =this.state;
         return (
-            <div class="game-container">
-
-                
+            <div className="game-container">                
             </div>
         );
     }
